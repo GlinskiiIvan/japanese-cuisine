a14a0e22de7898acd80556bfd7a77eedaf205cb2
Glinsky Ivan <glinsky_ivan@mail.ru>
2022-07-13 18:26:45 +0600

    added section PromoText

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,14 @@
 import styles from './App.module.scss'
 import Header from "../Layout/Header/Header";
 import Banner from "../Layout/Banner/Banner";
+import PromoText from "../Layout/PromoText/PromoText";
 
 function App(props) {
   return (
     <div className="App">
       <Header />
         <Banner />
+        <PromoText />
     </div>
   );
 }
